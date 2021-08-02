7fd4a49607647d7daa46cb3e497984da772479e7
Kennyyauw <87774280+Kennyyauw@users.noreply.github.com>
2021-08-02 19:35:14 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# 材料:
+<p>arduino Uno,Nano 等..
+<p>RFID-RC522 (3.3v 電壓)
+<p>Servo (SG90)
+<p>RGB Led
+<p>Buzzer
---
+++ src/rfid_and_servo.ino
@@ -0,0 +1,74 @@
+#include <MFRC522.h>
+#include <Servo.h>
+#include <SPI.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+int red = 6;
+int green =7;
+int blue =8;
+int buzzer =2;
+
+MFRC522 mfrc522 (SS_PIN,RST_PIN);
+Servo myServo ;
+
+void setup(){
+  Serial.begin(9600);
+  myServo.attach (3);
+  SPI.begin();  
+  mfrc522.PCD_Init();
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(blue,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop (){
+  analogWrite(red,0);
+  analogWrite(green,0);
+  analogWrite(blue,255);
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return ;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return ;
+  }
+  Serial.println("uid tag:");
+  String content = "" ;
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  content.toUpperCase();
+  if(content.substring(1) == "4A 8A 95 B3")
+  { 
+    Serial.println("access ok");
+    Serial.println();
+    delay(500);
+    myServo.write(180);
+    analogWrite(red,0);
+    analogWrite(green,255);
+    analogWrite(blue,0);
+    delay(3000);
+    myServo.write(0);
+  }
+   else {
+    Serial.println("access denided");
+    delay(500);
+    analogWrite(red,255);
+    analogWrite(green,0);
+    analogWrite(blue,0);
+    digitalWrite(buzzer,HIGH);
+    delay(1000);
+    digitalWrite(buzzer,LOW);
+    delay(1000);
+    
+   }
+}
